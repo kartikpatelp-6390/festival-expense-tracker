492dce0f5e31f5883d8010812cb347a55c61bde0
kartik patel <kartikatelp@outlook.com>
2025-06-27 11:13:17 +0530

    Changes in docker files and configuration files

--- back-end/app.js
+++ back-end/app.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}));
 app.use(express.json());
 
 app.use("/api/house", protect, require("./routes/houseRoutes"));
---
+++ front-end/src/environments/environment.docker.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://backend:3000/api'  // Use Docker service name (backend)
+};
